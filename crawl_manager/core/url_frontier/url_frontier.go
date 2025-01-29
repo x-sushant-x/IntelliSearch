646d9e3e14faa451f3acda738a1e6f00c878e4fd
Sushant <sushant.dhiman9812@gmail.com>
2025-01-29 21:46:40 +0530

    Added code to push to kafka from URLFrontier

--- crawl_manager/core/url_frontier/url_frontier.go
+++ crawl_manager/core/url_frontier/url_frontier.go
@@ -1,13 +1,30 @@
 package urlfrontier
 
-import "github.com/x-sushant-x/IntelliSearch/crawl_manager/core/queue"
+import (
+	"github.com/x-sushant-x/IntelliSearch/crawl_manager/core/queue"
+	"log"
+)
+
+const (
+	crawlURLsKafkaTopic = "crawl-urls"
+)
 
 type URLFrontier struct {
-	messageQueue queue.MessageQueue
+	messageQueue  queue.MessageQueue
+	onGoingCrawls map[string]bool
 }
 
 func NewURLFrontier(queue queue.MessageQueue) URLFrontier {
 	return URLFrontier{
 		messageQueue: queue,
+	}
+}
+
+func (frontier URLFrontier) SendURLToQueueForCrawling(urls []string) {
+	for _, url := range urls {
+		err := frontier.messageQueue.Send(crawlURLsKafkaTopic, "", url)
+		if err != nil {
+			log.Default().Println("Unable to send crawling URL to queue:", err)
+		}
 	}
 }
